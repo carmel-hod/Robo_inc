5470fc24f183308e01bf5eda15ea14b076ac45aa
carmel-hod <carmel.n.hod@gmail.com>
2020-08-27 13:59:15 -0700

    all sowhs selling doesnt work but no action bar

--- app/src/main/java/robo/inc/MainActivity.java
+++ app/src/main/java/robo/inc/MainActivity.java
@@ -1,5 +1,6 @@
 package robo.inc;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -18,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView matrials;
     private TextView robots;
     private TextView hype;
+    private TextView sci;
+    private TextView money;
     private Game_Master gm = Game_Master.getInstance();
 
 
@@ -27,7 +30,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         matrials = findViewById(R.id.matriels);
         robots = findViewById(R.id.robots);
+        money = findViewById(R.id.money);
         hype = findViewById(R.id.hype);
+        sci = findViewById(R.id.sci);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -35,15 +40,21 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
         final Handler handler = new Handler(Looper.getMainLooper()   );
         handler.postDelayed(
                 new Runnable(){
                     public void run(){
-                        matrials.setText("Matrials:" + Integer.toString(gm.matrials));
-                        robots.setText("robots:" + Integer.toString(gm.robots));
-                        hype.setText("hype:" + Integer.toString(gm.advertise_points));
+                        matrials.setText("M:" + Integer.toString(gm.matrials));
+                        robots.setText("R:" + Integer.toString(gm.robots));
+                        money.setText("$:" + Integer.toString(gm.money));
+                        hype.setText("H:" + Integer.toString(gm.advertise_points));
+                        sci.setText("S:" + Integer.toString(gm.scince_points));
+                        robots.setTypeface(null, Typeface.BOLD);
+                        hype.setTypeface(null, Typeface.BOLD);
+                        money.setTypeface(null, Typeface.BOLD);
+                        sci.setTypeface(null, Typeface.BOLD);
+                        matrials.setTypeface(null, Typeface.BOLD);
                         handler.postDelayed(this, 100);
                     }
                 }, 100);
